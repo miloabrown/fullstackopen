7958cba985c3283e46a9921f1e67b3f0af193917
Milo Brown <miloabrown@live.com>
2018-10-29 21:45:14 +0200

    Make initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}!</p>
+    </div>
+  )
+}
+const Footer = () => {
+  return (
+    <div>greeting app created by <a href="https://github.com/mluukkai">mluukkai</a></div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Anniina"/>
+      <Footer />
+
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
